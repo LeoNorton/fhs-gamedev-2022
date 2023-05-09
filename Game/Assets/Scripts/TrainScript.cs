3d14922126b79f179ff95cf0d49f104460b31516
flameofanor <68980551+flameofanor@users.noreply.github.com>
2023-05-09 16:57:00 -0600

    train moose

---
+++ Game/Assets/Scripts/TrainScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+
+public class TrainScript : MonoBehaviour
+{
+
+    [SerializeField] private List<Button> trainStops;
+    private int stopNumber;
+    private int targetStop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void goToStop(int target)
+    {
+        Sequence s = DOTween.Sequence();
+        s.Append(transform.DORotate(new Vector3(0, target > stopNumber ? 0 : 180, 0), 0.5f));
+        while (target != stopNumber)
+        {
+            if (target > stopNumber)
+            {
+                stopNumber++;
+            }
+            else
+            {
+                stopNumber--;
+            }
+            s.Append(transform.DOMove(trainStops[stopNumber].transform.position, 1f));
+        }
+    }
+
+
+
+}
