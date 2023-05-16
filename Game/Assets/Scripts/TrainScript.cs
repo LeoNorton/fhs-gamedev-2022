134cda0b6fbfa11a425a57ea8fd9e0d13cb8d84b
flameofanor <68980551+flameofanor@users.noreply.github.com>
2023-05-16 16:32:39 -0600

    gg

--- Game/Assets/Scripts/TrainScript.cs
+++ Game/Assets/Scripts/TrainScript.cs
@@ -11,11 +11,12 @@ public class TrainScript : MonoBehaviour
     [SerializeField] private List<Button> trainStops;
     private int stopNumber;
     private int targetStop;
+    private bool traveling;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        traveling = false;
     }
 
     // Update is called once per frame
@@ -26,6 +27,8 @@ public class TrainScript : MonoBehaviour
 
     public void goToStop(int target)
     {
+        if (traveling) return;
+        traveling = true;
         Sequence s = DOTween.Sequence();
         s.Append(transform.DORotate(new Vector3(0, target > stopNumber ? 0 : 180, 0), 0.5f));
         while (target != stopNumber)
@@ -39,7 +42,9 @@ public class TrainScript : MonoBehaviour
                 stopNumber--;
             }
             s.Append(transform.DOMove(trainStops[stopNumber].transform.position, 1f));
+
         }
+        s.OnComplete(() => traveling = false);
     }
 
 
